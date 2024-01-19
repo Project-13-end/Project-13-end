a152045ab271fcdaa4236fc4da04e90cb99fe484
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-20 06:11:13 +0900

    fix: Post에 Member가 추가되지 않는 오류 수정

--- src/main/java/com/ll/project_13_backend/post/dto/service/CreatePostDto.java
+++ src/main/java/com/ll/project_13_backend/post/dto/service/CreatePostDto.java
@@ -1,5 +1,6 @@
 package com.ll.project_13_backend.post.dto.service;
 
+import com.ll.project_13_backend.member.entity.Member;
 import com.ll.project_13_backend.post.entity.Category;
 import com.ll.project_13_backend.post.entity.Post;
 import lombok.Builder;
@@ -7,8 +8,9 @@ import lombok.Builder;
 @Builder
 public record CreatePostDto(String title, String content, Category category, Long price) {
 
-    public Post createPostDtoToPost() {
+    public Post createPostDtoToPost(Member member) {
         Post post = Post.builder()
+                .member(member)
                 .title(title)
                 .content(content)
                 .category(category)
