617272f88a112ab8325da2c753a563b6c92e5a68
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-20 05:59:24 +0900

    feat: 단일 게시글 조회 기능 추가

--- src/main/java/com/ll/project_13_backend/post/service/PostService.java
+++ src/main/java/com/ll/project_13_backend/post/service/PostService.java
@@ -2,8 +2,11 @@ package com.ll.project_13_backend.post.service;
 
 import com.ll.project_13_backend.member.entity.Member;
 import com.ll.project_13_backend.post.dto.service.CreatePostDto;
+import com.ll.project_13_backend.post.dto.service.FindPostDto;
 
 public interface PostService {
 
     Long createPost(final CreatePostDto createPostDto, final Member member);
+
+    FindPostDto findPost(final Long postId);
 }
--- src/main/java/com/ll/project_13_backend/post/service/PostServiceImpl.java
+++ src/main/java/com/ll/project_13_backend/post/service/PostServiceImpl.java
@@ -1,9 +1,11 @@
 package com.ll.project_13_backend.post.service;
 
 import com.ll.project_13_backend.global.exception.AuthException;
+import com.ll.project_13_backend.global.exception.EntityNotFoundException;
 import com.ll.project_13_backend.global.exception.ErrorCode;
 import com.ll.project_13_backend.member.entity.Member;
 import com.ll.project_13_backend.post.dto.service.CreatePostDto;
+import com.ll.project_13_backend.post.dto.service.FindPostDto;
 import com.ll.project_13_backend.post.entity.Post;
 import com.ll.project_13_backend.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +22,19 @@ public class PostServiceImpl implements PostService {
     @Transactional
     public Long createPost(final CreatePostDto createPostDto, final Member member) {
         checkLogin(member);
-        Post post = createPostDto.createPostDtoToPost();
+        //todo member객체를 넣어도 괜찮나 더 좋은 방법이 없나에 대해 생각
+        Post post = createPostDto.createPostDtoToPost(member);
         Post id = postRepository.save(post);
 
         return id.getId();
+    }
+
+    public FindPostDto findPost(final Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.ENTITY_NOT_FOUND));
+
+        FindPostDto findPostDto = FindPostDto.of(post);
+        return findPostDto;
     }
 
     private void checkLogin(Member member) {
