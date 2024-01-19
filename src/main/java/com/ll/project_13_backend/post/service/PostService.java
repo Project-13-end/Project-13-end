52253fd2b8b9e590e30b3fe8d483d23193a69e7f
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-19 21:56:46 +0900

    feat: Post를 데이터베이스에 save하는 기능 추가

--- src/main/java/com/ll/project_13_backend/post/service/PostService.java
+++ src/main/java/com/ll/project_13_backend/post/service/PostService.java
@@ -1,4 +1,9 @@
 package com.ll.project_13_backend.post.service;
 
+import com.ll.project_13_backend.member.entity.Member;
+import com.ll.project_13_backend.post.dto.service.CreatePostDto;
+
 public interface PostService {
+
+    Long createPost(final CreatePostDto createPostDto, final Member member);
 }
--- src/main/java/com/ll/project_13_backend/post/service/PostServiceImpl.java
+++ src/main/java/com/ll/project_13_backend/post/service/PostServiceImpl.java
@@ -1,7 +1,35 @@
 package com.ll.project_13_backend.post.service;
 
+import com.ll.project_13_backend.global.exception.AuthException;
+import com.ll.project_13_backend.global.exception.ErrorCode;
+import com.ll.project_13_backend.member.entity.Member;
+import com.ll.project_13_backend.post.dto.service.CreatePostDto;
+import com.ll.project_13_backend.post.entity.Post;
+import com.ll.project_13_backend.post.repository.PostRepository;
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 @Service
 public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Transactional
+    public Long createPost(final CreatePostDto createPostDto, final Member member) {
+        checkLogin(member);
+        Post post = createPostDto.createPostDtoToPost();
+        Post id = postRepository.save(post);
+
+        return id.getId();
+    }
+
+    private void checkLogin(Member member) {
+        if (member == null) {
+            throw new AuthException(ErrorCode.IS_NOT_LOGIN);
+        }
+    }
 }
