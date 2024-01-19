0d41a76e6b44a48c747929538057bbaee235ffd5
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-20 06:07:20 +0900

    feat: findPost에서 사용할 dto 추가

---
+++ src/main/java/com/ll/project_13_backend/post/dto/service/FindPostDto.java
@@ -0,0 +1,27 @@
+package com.ll.project_13_backend.post.dto.service;
+
+import com.ll.project_13_backend.post.entity.Post;
+import java.time.LocalDateTime;
+import lombok.Builder;
+
+@Builder
+public record FindPostDto(String name,
+                          String title,
+                          String content,
+                          String category,
+                          Long price,
+                          LocalDateTime createdDate) {
+
+    public static FindPostDto of(Post post) {
+        FindPostDto findPostDto = FindPostDto.builder()
+                .name(post.getMember().getName())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .category(post.getCategory().getValue())
+                .price(post.getPrice())
+                .createdDate(post.getCreatedDate())
+                .build();
+
+        return findPostDto;
+    }
+}
