bd34b0a999dae731fad994dacb768d8a208bb30c
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-19 21:54:37 +0900

    feat: Entity를 반환하는 save메서드 추가

--- src/main/java/com/ll/project_13_backend/post/repository/PostRepository.java
+++ src/main/java/com/ll/project_13_backend/post/repository/PostRepository.java
@@ -4,4 +4,5 @@ import com.ll.project_13_backend.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Post save(Post post);
 }
