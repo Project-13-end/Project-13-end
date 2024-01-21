cfda8612f3984af3c632f8a9a3b2162a10e091b6
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-21 23:25:21 +0900

    feat: validator 설정

--- src/main/java/com/ll/project_13_backend/post/dto/request/CreatePostRequest.java
+++ src/main/java/com/ll/project_13_backend/post/dto/request/CreatePostRequest.java
@@ -2,10 +2,14 @@ package com.ll.project_13_backend.post.dto.request;
 
 import com.ll.project_13_backend.post.dto.service.CreatePostDto;
 import com.ll.project_13_backend.post.entity.Category;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
-public record CreatePostRequest(String title, String content, String category, Long price) {
+public record CreatePostRequest(@NotBlank(message = "제목을 반드시 입력해주세요.") String title,
+                                @NotBlank(message = "내용을 반드시 입력해주세요.") String content,
+                                @NotBlank(message = "카테고리를 반드시 선택해주세요.") String category,
+                                @NotBlank(message = "가격을 반드시 입력해주세요.") Long price) {
 
     public CreatePostDto createPostRequestTocreatePostDto() {
         CreatePostDto createPostDto = CreatePostDto.builder()
