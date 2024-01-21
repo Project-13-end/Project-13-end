13c5561bea527b46082fead0f20378bdede4ed57
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-21 23:26:00 +0900

    feat: LoginId로 Member를 찾는 기능 추가

--- src/main/java/com/ll/project_13_backend/member/repository/MemberRepository.java
+++ src/main/java/com/ll/project_13_backend/member/repository/MemberRepository.java
@@ -1,11 +1,9 @@
 package com.ll.project_13_backend.member.repository;
 
 import com.ll.project_13_backend.member.entity.Member;
-import io.jsonwebtoken.security.Jwks;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByLoginId(String loginId);
+    Optional<Member> findByLoginId(String loginId);
 }
