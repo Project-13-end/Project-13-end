f3372e9a352a0dc883ece329ec01614b9ea722dd
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-21 23:47:36 +0900

    fix: price의 검증이 안되는 오류 수정

--- src/main/java/com/ll/project_13_backend/post/dto/request/CreatePostRequest.java
+++ src/main/java/com/ll/project_13_backend/post/dto/request/CreatePostRequest.java
@@ -3,13 +3,14 @@ package com.ll.project_13_backend.post.dto.request;
 import com.ll.project_13_backend.post.dto.service.CreatePostDto;
 import com.ll.project_13_backend.post.entity.Category;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record CreatePostRequest(@NotBlank(message = "제목을 반드시 입력해주세요.") String title,
                                 @NotBlank(message = "내용을 반드시 입력해주세요.") String content,
                                 @NotBlank(message = "카테고리를 반드시 선택해주세요.") String category,
-                                @NotBlank(message = "가격을 반드시 입력해주세요.") Long price) {
+                                @NotNull Long price) {
 
     public CreatePostDto createPostRequestTocreatePostDto() {
         CreatePostDto createPostDto = CreatePostDto.builder()
