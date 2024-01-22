56d7aeef2155667feeb8d15cd952692931b37dc1
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-22 16:22:55 +0900

    feat: builder패턴 추가

--- src/main/java/com/ll/project_13_backend/post/dto/request/UpdatePostRequest.java
+++ src/main/java/com/ll/project_13_backend/post/dto/request/UpdatePostRequest.java
@@ -2,7 +2,9 @@ package com.ll.project_13_backend.post.dto.request;
 
 import com.ll.project_13_backend.post.dto.service.UpdatePostDto;
 import com.ll.project_13_backend.post.entity.Category;
+import lombok.Builder;
 
+@Builder
 public record UpdatePostRequest(String title, String content, String category, Long price) {
 
     public UpdatePostDto toUpdatePostDto() {
