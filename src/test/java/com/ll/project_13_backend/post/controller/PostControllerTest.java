22faf209573bac90031c6ca5dfe5faa6dc6f6a9b
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-22 16:57:00 +0900

    test: 게시글 수정 시 카테고리를 반드시 선택해야한다.

--- src/test/java/com/ll/project_13_backend/post/controller/PostControllerTest.java
+++ src/test/java/com/ll/project_13_backend/post/controller/PostControllerTest.java
@@ -278,7 +278,7 @@ class PostControllerTest {
                         jsonPath("$.message").value("적절하지 않은 요청 값입니다."),
                         jsonPath("$.errors[0].field").value("title"),
                         jsonPath("$.errors[0].value").isEmpty(),
-                        jsonPath("$.errors[0].message").value("제목을 반드시 입력해주세요")
+                        jsonPath("$.errors[0].message").value("제목을 반드시 입력해주세요.")
                 );
     }
 
@@ -302,7 +302,31 @@ class PostControllerTest {
                         jsonPath("$.message").value("적절하지 않은 요청 값입니다."),
                         jsonPath("$.errors[0].field").value("content"),
                         jsonPath("$.errors[0].value").isEmpty(),
-                        jsonPath("$.errors[0].message").value("내용을 반드시 입력해주세요")
+                        jsonPath("$.errors[0].message").value("내용을 반드시 입력해주세요.")
+                );
+    }
+
+    @DisplayName("게시글 수정 시 카테고리를 반드시 선택해야한다.")
+    @Test
+    public void updatePostNotSelectCategoryTest() throws Exception {
+        //given
+        UpdatePostRequest updatePostRequest = UpdatePostRequest.builder()
+                .title("titleTest1")
+                .content("contentTest1")
+                .price(10000L)
+                .build();
+
+        //when & then
+        mockMvc.perform(put("/post/{postId}", 1L)
+                        .content(objectMapper.writeValueAsString(updatePostRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpectAll(
+                        jsonPath("$.code").value("C_002"),
+                        jsonPath("$.message").value("적절하지 않은 요청 값입니다."),
+                        jsonPath("$.errors[0].field").value("category"),
+                        jsonPath("$.errors[0].value").isEmpty(),
+                        jsonPath("$.errors[0].message").value("카테고리를 반드시 선택해주세요.")
                 );
     }
 }
