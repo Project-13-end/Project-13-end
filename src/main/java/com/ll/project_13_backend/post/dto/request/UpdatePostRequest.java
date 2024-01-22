4d23b49f7cc07b517bcf6ad9c1bfe455fc682a44
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-22 16:56:15 +0900

    refactor: 오타 수정

--- src/main/java/com/ll/project_13_backend/post/dto/request/UpdatePostRequest.java
+++ src/main/java/com/ll/project_13_backend/post/dto/request/UpdatePostRequest.java
@@ -7,9 +7,9 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
-public record UpdatePostRequest(@NotBlank(message = "제목을 반드시 입력해주세요") String title,
-                                @NotBlank(message = "내용을 반드시 입력해주세요") String content,
-                                @NotBlank(message = "카테고리를 반드시 입력해주세요.") String category,
+public record UpdatePostRequest(@NotBlank(message = "제목을 반드시 입력해주세요.") String title,
+                                @NotBlank(message = "내용을 반드시 입력해주세요.") String content,
+                                @NotBlank(message = "카테고리를 반드시 선택해주세요.") String category,
                                 @NotNull(message = "가격을 반드시 입력해주세요.") Long price) {
 
     public UpdatePostDto toUpdatePostDto() {
