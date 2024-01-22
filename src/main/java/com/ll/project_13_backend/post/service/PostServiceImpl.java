4761eff54a258a1340a7e92834b5a5fb2b59a579
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-23 05:27:06 +0900

    refactor: 의존성 줄임

--- src/main/java/com/ll/project_13_backend/post/service/PostServiceImpl.java
+++ src/main/java/com/ll/project_13_backend/post/service/PostServiceImpl.java
@@ -4,6 +4,7 @@ import com.ll.project_13_backend.global.exception.AuthException;
 import com.ll.project_13_backend.global.exception.EntityNotFoundException;
 import com.ll.project_13_backend.global.exception.ErrorCode;
 import com.ll.project_13_backend.member.entity.Member;
+import com.ll.project_13_backend.member.repository.MemberRepository;
 import com.ll.project_13_backend.post.dto.service.CreatePostDto;
 import com.ll.project_13_backend.post.dto.service.FindPostDto;
 import com.ll.project_13_backend.post.dto.service.UpdatePostDto;
@@ -19,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional
     public Long createPost(final CreatePostDto createPostDto, final Member member) {
@@ -40,29 +42,32 @@ public class PostServiceImpl implements PostService {
 
     @Transactional
     public void updatePost(final Long postId, final UpdatePostDto updatePostDto, final Member member) {
-        checkAuthorized(member);
+        checkAnonymous(member);
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new EntityNotFoundException(ErrorCode.ENTITY_NOT_FOUND));
+        post.checkAuthorized(member);
         post.updatePost(updatePostDto);
     }
 
     @Transactional
     public void deletePost(final Long postId, final Member member) {
-        checkAuthorized(member);
+        checkAnonymous(member);
 //        if (!postRepository.existsById(postId)) { //todo 원하지 않는 예외가 발생함 원인을 모름 물어보자
 //            throw new EntityNotFoundException(ErrorCode.ENTITY_NOT_FOUND);
 //        }
 //        postRepository.deleteById(postId);
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new EntityNotFoundException(ErrorCode.ENTITY_NOT_FOUND));
+        post.checkAuthorized(member);
         postRepository.delete(post);
     }
 
-    private static void checkAuthorized(Member member) {
+    private static void checkAnonymous(Member member) {
         if (member == null) {
             throw new AuthException(ErrorCode.UNAUTHORIZED_USER);
         }
     }
+
     private void checkLogin(Member member) {
         if (member == null) {
             throw new AuthException(ErrorCode.IS_NOT_LOGIN);
