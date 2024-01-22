06bfd19222a8fca6bed582b99a0a5ac27930e365
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-22 16:41:20 +0900

    feat: validator 기능 추가

--- src/main/java/com/ll/project_13_backend/post/dto/request/UpdatePostRequest.java
+++ src/main/java/com/ll/project_13_backend/post/dto/request/UpdatePostRequest.java
@@ -2,10 +2,15 @@ package com.ll.project_13_backend.post.dto.request;
 
 import com.ll.project_13_backend.post.dto.service.UpdatePostDto;
 import com.ll.project_13_backend.post.entity.Category;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
-public record UpdatePostRequest(String title, String content, String category, Long price) {
+public record UpdatePostRequest(@NotBlank(message = "제목을 반드시 입력해주세요") String title,
+                                @NotBlank(message = "내용을 반드시 입력해주세요") String content,
+                                @NotBlank(message = "카테고리를 반드시 입력해주세요.") String category,
+                                @NotNull(message = "가격을 반드시 입력해주세요.") Long price) {
 
     public UpdatePostDto toUpdatePostDto() {
         UpdatePostDto updatePostDto = UpdatePostDto.builder()
